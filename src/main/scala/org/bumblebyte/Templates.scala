eccfb69faea80ab808644dd2cc9c0f2df54de11f
Adam King <adam.king@bumblebyte.co.uk>
2024-11-29 08:26:32 +0000

    Add styling

--- src/main/scala/org/bumblebyte/Templates.scala
+++ src/main/scala/org/bumblebyte/Templates.scala
@@ -2,24 +2,36 @@ package org.bumblebyte
 
 import scalatags.Text.all.*
 import scalatags.Text.tags2
+import scalatags.stylesheet.Cls
+import scalatags.stylesheet.StyleSheet
 
 object Templates:
 
-  private val LegalDisclaimer =
-    """
-      |BumbleByte Software Ltd is a company registered in England and Wales. Registered number: 16009956. Registered
-      |office: 3rd Floor, 86-90 Paul Street, London, United Kingdom, EC2A 4NE.
-      |""".stripMargin
+  private object Style extends StyleSheet:
+
+    initStyleSheet()
+
+    val base: Cls = cls()
 
   def base(pageName: String, contents: Seq[Frag]): Frag =
     html(
       lang := "en",
       head(
-        meta(name := "copyright", content := "© 2024 Your Name"),
+        link(rel  := "stylesheet", href   := "https://cdn.simplecss.org/simple.min.css"),
+        meta(name := "copyright", content := "© 2024 BumbleByte Software Ltd"),
         meta(name := "license", content   := "https://creativecommons.org/licenses/by-nc-nd/4.0/"),
         tags2.title(pageName)
       ),
-      body(a(href := "index.html")(h1("BumbleByte Software")), h2("Independent Software Contracting"))(contents)(
-        footer(LegalDisclaimer)
+      body(
+        Style.base,
+        header(a(href := "index.html")(h1("BumbleByte Software")), h2("Independent Software Contracting")),
+        tags2.main(contents*),
+        footer(
+          p("BumbleByte Software Ltd is a company registered in England and Wales. Registered number: 16009956. Registered office: 3rd Floor, 86-90 Paul Street, London, United Kingdom, EC2A 4NE."),
+          p(
+            "© 2024 BumbleByte Software Ltd. Blog content is licensed under a Creative Commons Attribution-NonCommercial-NoDerivs (CC BY-NC-ND) license. ",
+            a(href := "https://creativecommons.org/licenses/by-nc-nd/4.0/", "Learn more.")
+          )
+        )
       )
     )
--- src/main/scala/org/bumblebyte/generate.scala
+++ src/main/scala/org/bumblebyte/generate.scala
@@ -6,6 +6,7 @@ import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
 import scalatags.Text
 import scalatags.Text.all.*
+import scalatags.Text.tags2
 
 import java.io.InputStreamReader
 import java.time.LocalDate
@@ -13,32 +14,40 @@ import java.time.temporal.ChronoUnit
 import java.util.Collections
 import scala.util.chaining.*
 
+case class BlogPost(title: String, publishedDate: LocalDate, filename: String)
+
 @main
 def generate(): Unit =
   val extensions = Collections.singletonList(YamlFrontMatterExtension.create())
   val parser     = Parser.builder().extensions(extensions).build()
   val renderer   = HtmlRenderer.builder().escapeHtml(false).build()
 
-  val strings = os.walk(os.pwd / "posts", skip = _.ext != "md").map { path =>
-    val inputStream = os.read.inputStream(path)
-    val node        = parser.parseReader(new InputStreamReader(inputStream))
+  val strings = os
+    .walk(os.pwd / "posts", skip = _.ext != "md")
+    .map { path =>
+      val inputStream = os.read.inputStream(path)
+      val node        = parser.parseReader(new InputStreamReader(inputStream))
 
-    val metadata = new YamlFrontMatterVisitor().tap(node.accept).pipe(_.getData)
-    val html     = renderer.render(node)
+      val metadata = new YamlFrontMatterVisitor().tap(node.accept).pipe(_.getData)
+      val html     = renderer.render(node)
 
-    val publishedDate = Option(metadata.get("date")).fold(sys.error(s"$path had no `publishedDate`"))(_.getFirst)
+      val publishedDate =
+        LocalDate.parse(Option(metadata.get("date")).fold(sys.error(s"$path had no `publishedDate`"))(_.getFirst))
 
-    val file = Templates.base(path.baseName, Seq(h1(path.baseName), raw(html)))
-    os.write.over(os.pwd / s"$publishedDate.html", file.render)
-    path.baseName -> s"$publishedDate.html"
-  }
+      val file = Templates.base(path.baseName, Seq(h1(path.baseName), raw(html)))
+      os.write.over(os.pwd / s"$publishedDate.html", s"<!DOCTYPE html>${file.render}")
+      BlogPost(path.baseName, publishedDate, s"$publishedDate.html")
+    }
+    .sortBy(_.publishedDate)(Ordering[LocalDate].reverse)
 
   val yoe = 1 + ChronoUnit.YEARS.between(LocalDate.of(2017, 9, 1), LocalDate.now())
   val index = Seq(
     p(s"Polyglot software engineer with $yoe years of experience."),
-    ul(strings.map { (title, fileRef) =>
-      li(a(href := fileRef)(title))
-    }*)
+    tags2.nav(
+      ul(strings.map { case BlogPost(title, publishedDate, fileRef) =>
+        li(a(attr("aria-current") := "page", href := fileRef)(s"$publishedDate - $title"))
+      }*)
+    )
   )
 
   val `index.html` =
